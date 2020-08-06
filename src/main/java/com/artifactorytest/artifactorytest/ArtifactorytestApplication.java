29961f6cb9c62fe76ad33e6acb07b2fd2fc3df5d
Shubha <shubha.acharya@cognizant.com>
2020-08-06 19:21:01 +0530

    first commit

---
+++ src/main/java/com/artifactorytest/artifactorytest/ArtifactorytestApplication.java
@@ -0,0 +1,13 @@
+package com.artifactorytest.artifactorytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactorytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactorytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artifactorytest/artifactorytest/ArtifactorytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artifactorytest.artifactorytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactorytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
